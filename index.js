9cf37e82a7831282e7d97093d132212aa81c15cc
xaviguasch <xaviguasch@gmail.com>
2020-08-12 18:41:04 +0200

    added initial files

---
+++ index.js
@@ -0,0 +1,16 @@
+// create a promise
+// wait for 4 seconds
+// resolve or reject based on randomBool
+// false - reject 'hero is on vacation'
+// true - resolve 'batman is here'
+
+const randomBool = Boolean(Math.round(Math.random()))(
+  // returns either 0 or 1
+
+  async function () {
+    try {
+    } catch (err) {
+      console.log(err)
+    }
+  }
+)()
